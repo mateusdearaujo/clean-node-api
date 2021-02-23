9352f2260df2520c7c6769730816514d0ffcdf23
Mateus Araujo <mateusdearaujo@hotmail.com.br>
2021-02-23 17:01:54 -0300

    feat: ensure AccountMongoRepository returns an account on success

--- jest-mongodb-config.js
+++ jest-mongodb-config.js
@@ -1,12 +1,10 @@
 module.exports = {
     mongodbMemoryServerOptions: {
         binary: {
-            version: '4.0.3',
+            version: '4.4.1',
             skipMD5: true
         },
         autoStart: false,
-        instance: {
-            dbName: 'jest'
-        }
+        instance: {}
     }
-};
+}
